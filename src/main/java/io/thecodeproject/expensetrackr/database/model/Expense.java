303a7480cdd96ab7d0676416dd3a5dee00145338
nagarajarchak <archak.nagaraj@gmail.com>
2023-07-28 21:43:40 +1000

    Added mongodb model for expense collection

---
+++ src/main/java/io/thecodeproject/expensetrackr/database/model/Expense.java
@@ -0,0 +1,35 @@
+package io.thecodeproject.expensetrackr.database.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+@Getter
+@Setter
+@ToString
+public class Expense {
+
+    @Id
+    private String id;
+
+    @Field("Title")
+    private String title;
+
+    @Field("Description")
+    private String description;
+
+    @Field("Type")
+    private String type;
+
+    @Field("Amount")
+    private double amount;
+
+    @Field("Timestamp")
+    private String timestamp;
+
+    @Field("Comment")
+    private String comment;
+
+}
