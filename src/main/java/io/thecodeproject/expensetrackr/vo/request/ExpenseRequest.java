2859fc70a43057571e38b742d3d57b22c4d889dc
nagarajarchak <archak.nagaraj@gmail.com>
2023-07-29 20:04:18 +1000

    Added getter, setter and to string annotations

--- src/main/java/io/thecodeproject/expensetrackr/vo/request/ExpenseRequest.java
+++ src/main/java/io/thecodeproject/expensetrackr/vo/request/ExpenseRequest.java
@@ -1,8 +1,14 @@
 package io.thecodeproject.expensetrackr.vo.request;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Field;
 
+@Getter
+@Setter
+@ToString
 public class ExpenseRequest {
     private String title;
     private String description;
