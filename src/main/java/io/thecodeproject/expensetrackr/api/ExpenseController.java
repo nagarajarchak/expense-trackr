9984d94412ade431abcf40bb4a8254973fe5d496
nagarajarchak <archak.nagaraj@gmail.com>
2023-07-29 20:01:14 +1000

    Reference from constant for endpoint base

--- src/main/java/io/thecodeproject/expensetrackr/api/ExpenseController.java
+++ src/main/java/io/thecodeproject/expensetrackr/api/ExpenseController.java
@@ -1,12 +1,13 @@
 package io.thecodeproject.expensetrackr.api;
 
+import io.thecodeproject.expensetrackr.constant.RequestConstant;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/expense")
+@RequestMapping(RequestConstant.EXPENSE_BASE)
 public class ExpenseController {
 
     private static final Logger LOG = LoggerFactory.getLogger(ExpenseController.class);
