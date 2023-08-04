d6eea3005bbd5630547d406fc92238328b9c3b65
nagarajarchak <archak.nagaraj@gmail.com>
2023-08-04 23:44:29 +1000

    Added result object

---
+++ src/main/java/io/thecodeproject/expensetrackr/vo/response/generic/Result.java
@@ -0,0 +1,21 @@
+package io.thecodeproject.themes.vo.response.generic;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@ToString
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public class Result {
+
+    private Integer response;
+    private String description;
+    private List<Errors> errors = new ArrayList<>();
+
+}
