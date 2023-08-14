186478502a480dbf41e33da0aa94bed04c215f7f
nagarajarchak <archak.nagaraj@gmail.com>
2023-08-14 20:50:12 +1000

    Adding request endpoint class

--- src/main/java/io/thecodeproject/expensetrackr/constant/RequestConstant.java
+++
@@ -1,7 +0,0 @@
-package io.thecodeproject.expensetrackr.constant;
-
-public class RequestConstant {
-
-    public static final String EXPENSE_BASE = "/expense";
-
-}
---
+++ src/main/java/io/thecodeproject/expensetrackr/constant/RequestEndpoint.java
@@ -0,0 +1,9 @@
+package io.thecodeproject.expensetrackr.constant;
+
+public class RequestEndpoint {
+
+    public static final String EXPENSE_TRACKR_BASE = "/expense-trackr";
+    public static final String REGISTER_EXPENSE = "/expense";
+    public static final String LIST_ALL_EXPENSE = "/expense";
+
+}
